d2bb6e5e36b49946fd0391c7f28c18ce28709545
clarice_r <claricerdallorto@gmail.com>
2021-12-20 10:37:23 -0300

    Web and api routes files acting out

--- routes/api.php
+++ routes/api.php
@@ -17,8 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-<<<<<<< HEAD
-=======
-
-Route::apiResource('pets', PetController::class);
->>>>>>> parent of f3d54dc (Creating model again)
